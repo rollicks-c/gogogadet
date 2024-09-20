f43c83e99dc8e9109f2afa41cc507e3fe5cb8bb6
Roli Christen <roland.christen@jaywalker.ch>
2024-09-21 00:05:27 +0200

    fuzzy search

--- funk/query.go
+++ funk/query.go
@@ -1,5 +1,7 @@
 package funk
 
+import "strings"
+
 func Contains[T comparable](pool []T, item T) bool {
 	for _, p := range pool {
 		if p == item {
@@ -7,4 +9,14 @@ func Contains[T comparable](pool []T, item T) bool {
 		}
 	}
 	return false
+}
+
+func FuzzySearch(exp string, pool []string) []string {
+	sel := make([]string, 0)
+	for _, p := range pool {
+		if strings.HasPrefix(p, exp) {
+			sel = append(sel, p)
+		}
+	}
+	return sel
 }
